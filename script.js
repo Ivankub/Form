a0b666a312a981ef5b149b2a82e257dce9df3387
Ivankub <vanadolgun@gmail.com>
2018-11-25 12:14:48 +0700

    SuperStory

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function sumbitAction(form) {
-	alert("សួស្តី, " + form.nameInput.value);
+	var story = document.getElementById("Story");
+	var game = document.getElementById("Game");
+	var game2 = document.getElementById("Game2");
+	var person = document.getElementById("personaz");
+	var predmet = document.getElementById("predmet");
+
+	story.innerHTML = form.nameInput.value;
+	game.innerHTML = form.whatGame.value;
+	game2.innerHTML = form.whatGame.value;
+	person.innerHTML = form.Person.value;
+	predmet.innerHTML = form.predmety.value;
 }
